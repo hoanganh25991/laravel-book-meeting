5d7023893901e1fb1bc6b52f1b1f06dd1b590cfc
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-10-04 14:03:22 +0700

    flash.js, improve, allow html

--- resources/assets/js/flash.js
+++ resources/assets/js/flash.js
@@ -1,19 +1,20 @@
 let endAnimationSignal = 'animationend';
 let animation = 'fadeOutRight';
+let flashDiv = $('.alert');
 let flash = function(msg, level){
 	level = level || 'info';
-	let flashDiv = $('.alert');
-	
-	flashDiv.text(msg);
-	flashDiv.removeClass();
-	flashDiv.addClass(`alert alert-${level}`);
+
+	flashDiv.html(msg);
+	flashDiv.attr('class', `alert alert-${level}`);
+
 	let interval = setInterval(()=>{
 		flashDiv.addClass(`animated  ${animation}`);
 		clearInterval(interval);
 	}, 3000);
 
 	flashDiv.one(endAnimationSignal, ()=>{
-		flashDiv.removeClass(`${animation}`);
+		// flashDiv.removeClass();
+		console.log('animation end');
 	});
 };
 window.flash = flash;
--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -11,11 +11,12 @@
                 <div class="input-group">
                     <pre class="input-group">{{ $group }}</pre>
                     <a group-id="{{ $group->id }}"
+                       group-name="{{ $group->name }}"
                     <?php
                         $attr = "class='my-addon btn btn-info'";
-                        if($group->user_status == 'join'){
-                            $attr .= "disabled";
-                        }
+//                        if($group->user_status == 'join'){
+//                            $attr .= "disabled";
+//                        }
                         echo $attr;
                     ?>>{{ $group->user_status }}</a>
                 </div>
@@ -26,29 +27,32 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="{{ url('js/flash.js') }}"></script>
     <script>
-        $('#groupList').on('click', 'a.my-addon', function(){
-            let btn = $(this);
-            let status = btn.text();
-            console.log(status);
-            if(status == 'pending'){
-//                alert('Please wait for group-host accept it');
-                flash('Please wait for group-host accept it');
-                return;
-            }
-            let group_id = btn.attr('group-id');
-            console.log(group_id);
-            $.post({
-                url: '/group/join',
-                data: {
-                    group_id: group_id
-                },
-                success: function(res){
-                    console.log(res);
-                    btn.text('pending');
-                },
-                error: function(res){
-                    console.log(res);
+        $(document).ready(function(){
+            $('#groupList').on('click', 'a.my-addon', function(){
+                let btn = $(this);
+                let status = btn.text();
+                console.log(status);
+                if(status == 'pending'){
+                    flash('Please wait for group-host accept it');
+                    return;
                 }
+                let group_id = btn.attr('group-id');
+                let group_name = btn.attr('group-name');
+                console.log(group_id);
+                $.post({
+                    url: "{{ url('group/join') }}",
+                    data: {
+                        group_id: group_id
+                    },
+                    success: function(res){
+                        console.log(res);
+                        btn.text('pending');
+                        flash(`Have ask <strong>${group_name}</strong>-host, please wait for accept`);
+                    },
+                    error: function(res){
+                        console.log(res);
+                    }
+                });
             });
         });
     </script>
